a2cf6a9d33c76f06bf831241087055171419a4a1
WangJunYoung <logtve@naver.com>
2022-11-23 23:43:10 +0900

    a
    a

--- crawling.ts
+++ crawling.ts
@@ -7,22 +7,24 @@ const address = 'https://playtoearn.net/blockchaingames?sort=socialscore_24h&dir
 //PageList받기
 //tbody_list안에 있는 게임 이름 50개 받아오기
 //For 이용, 1~35페이지. 1~50번째 게임 이름 받아오기.
-//다른 정보들도 같이 받아오기.
+//--다른 정보들도 같이 받아오기.
 
 //엑셀 문서작성
+(async () => {
+    let testList: string[] = [];
 
-const Test = (async () => {
     const client = applyCaseMiddleware(axios.create());
     const Req = await client.get(address);
     const $ = cheerio.load(Req.data);
-    const $PageList = $('ul[class=pageination]').last().html();
-    const CalledData = $('div[class=dapp_name]').html();
+    const $PageList = $('a[class=page-link]').each(function(i, element) {
 
-    console.log($PageList);
-
+    });
+    const CalledData = $('div[class=dapp_name]').each(function(i, element) {
+        testList[i] = $(element).text();
+    });
+    console.log(testList);
 })();
 
-Test;
 
 
 
