855c673acf0a3e55043abec264b6f4f62b9c04fb
WangJunYoung <logtve@naver.com>
2022-12-01 13:28:57 +0900

    s
    s

--- Type/crawling.ts
+++ Type/crawling.ts
@@ -1,6 +1,6 @@
 import applyCaseMiddleware from 'axios-case-converter';
 import axios from 'axios';
-import * as cheerio from 'cheerio';
+import { load } from 'cheerio';
 
 const address = 'https://playtoearn.net/blockchaingames?sort=socialscore_24h&direction=desc&page=';
 
@@ -10,30 +10,47 @@ const address = 'https://playtoearn.net/blockchaingames?sort=socialscore_24h&dir
 //--다른 정보들도 같이 받아오기.
 //엑셀 문서작성
 
+//IIFE
 (async () => {
-    let testList: string[] = [];
-    let pageNum = 1;
+    let pageNum = 1; 
+
 
     const client = applyCaseMiddleware(axios.create());
 
     //axios (address) with cheerio for page
-    const req = await client.get(address+pageNum);
-    const $ = cheerio.load(req.data);
+    const req = await client.get(address+pageNum); // 1
+    const $ = load(req.data);
 
     //Get PageNumber str to num
     const $page = $('a[class=page-link]').eq(-2).text();
-    pageNum = Number($page);
+    pageNum = Number($page);  //pageNum = 36
 
+    type TypeOfInfo = {
+        gameTitle: string[][],
+        gameBlockChain: string[][]
+    }
+
+    const tableSector = 'div[class=indexmaintable.indexpage] > tbody';
 
     //Get axios from 1 to pageNum
     for(let page = 1; page <= pageNum; page++){
-        const titleReq = await client.get(address+page);
-        const $$ = cheerio.load(titleReq.data);
+        const InfoRequest = await client.get(address+page);
+        const $ = load(InfoRequest.data);
+        
+        const gameInfo = $(tableSector).map((result) => {
+            gameTitle: $(result).find('div[class=dapp_name]').children('span:nth-child(1)').toArray().map((x) => { return $(x).text().trim()});
+            gameBlockChain: $(result).find('')
+            gameStatus: $(result).find('a[data-toggle=tooltip]')
+            gameNFT: $(result).find('')
+            gameF2P: $(result).find('')
+            gameP2E: $(result).find('')
+            gameSocial24h: $(result).find('span[class=dailychangepercentage]').text().trim();
+        })
 
-        const test = $$('div[class=dapp_name]').children('span:nth-child(1)').toArray().map((x) => { return $(x).text()});
-        testList.push(test)
+
         setTimeout(() => console.log(page + "번 페이지 진행 중\n"), 5000);
-    } 
+        
+    }
 })();
 
 /*
@@ -48,7 +65,36 @@ $('div[class=dapp_name]').children('span:nth-child(1)').each(function(i, element
 //    console.log("Number Of $Page, Type = " + $Page + " " + typeof $Page + "\nType, Page Num is = " + typeof PageNum + " " + PageNum);
 
 
+/*
 
+    let gameTitle: string[][] = [];
+    let gameBlockChain: string[][] = [];
+
+    type TypeOfInfo = {
+        gameTitle: string[][],
+        gameBlockChain: string[][]
+    }
+
+    let infoLists: TypeOfInfo ={
+        gameTitle: gameTitle,
+        gameBlockChain: gameBlockChain,
+
+    }
+
+        const InfoRequest = await client.get(address+page);
+        const $$ = cheerio.load(InfoRequest.data);
+
+        //use '.map', type ~ for get infos
+        const getTitleReq = $$('div[class=dapp_name]').children('span:nth-child(1)').toArray().map((x) => { return $$(x).text()});
+        gameTitle.push(getTitleReq);
+ 
+        const getBlockChainReq = $$('tbody > td:nth-child(5) > a[data-toggle=tooltip]').toArray().map((a) => { return $$(a).text()});
+        gameStatus.push(getBlockChainReq);
+        console.log(gameBlockChain);
+
+        const
+
+*/
 
 
 
